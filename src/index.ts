8751312feb26c2357c8630cb3529e48a99548491
Vitaliy <egoshinvit@gmail.com>
2025-04-04 15:21:04 +0700

    get requests

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,32 @@ import express, {Request, Response} from 'express'
 const app = express()
 const port = 3000
 
-app.get('/', (req: Request, res: Response) => {
-    let message = 'Hello Dimych!!!'
-    res.send(message)
+const users = [{id: '1', name: 'Dima'}, {id: '2', name: 'Ivan'}]
+const products = [{title: 'tomato'}, {title: 'milk'}]
+
+app.get('/users', (req: Request, res: Response) => {
+    res.send(users)
+})
+
+app.get('/users/:id', (req: Request, res: Response) => {
+    const user = users.find(x => x.id === req.params.id)
+    if (user) {
+        res.send(user)
+    } else {
+        res.send(404)
+    }
+})
+
+app.get('/products', (req: Request, res: Response) => {
+    res.send(products)
+})
+app.get('/products/:productTitle', (req: Request, res: Response) => {
+    let product = products.find(product => product.title === req.params.productTitle)
+    if (product) {
+        res.send(product)
+    } else {
+        res.send(404)
+    }
 })
 
 app.listen(port, () => {
