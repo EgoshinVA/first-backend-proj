5b445157e8e334c4f56eadd25fdf01b05edad9f6
Vitaliy <egoshinvit@gmail.com>
2025-04-05 10:21:12 +0700

    export products to router

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,14 @@
-import express, {Request, Response} from 'express'
+import express from 'express'
 import bodyParser from 'body-parser'
+import {productsRouter} from "./routes/products-route";
 
 const app = express()
 const port = 3000
 
-const products = [{id: 1, title: 'tomato'}, {id: 2, title: 'milk'}]
-
 app.use(bodyParser.json())
+app.use('/products', productsRouter)
 
-app.get('/products', (req: Request, res: Response) => {
-    if (req.query.title) {
-        const searchString = req.query.title.toString()
-        res.send(products.filter(p => p.title.includes(searchString)))
-    } else {
-        res.send(products)
-    }
-
-})
-app.get('/products/:id', (req: Request, res: Response) => {
-    let product = products.find(product => product.id === +req.params.id)
-    if (product) {
-        res.send(product)
-    } else {
-        res.send(404)
-    }
-})
-app.delete('/products/:id', (req: Request, res: Response) => {
-    for (let i = 0; i < products.length; i++) {
-        if (products[i].id === +req.params.id) {
-            products.splice(i, 1)
-            res.send(204)
-            return
-        }
-    }
-    res.send(404)
-})
-app.post('/products', (req: Request, res: Response) => {
-    const newId = products[products.length - 1].id + 1
-    const newProduct = {id: newId, title: req.body.title}
-    products.push(newProduct)
-    res.status(201).send(newProduct)
-})
-app.put('/products/:id', (req: Request, res: Response) => {
-    let product = products.find(x => x.id === +req.params.id)
-    if (product) {
-        product.title = req.body.title
-        res.send(product)
-    } else {
-        res.send(404)
-    }
-})
-
+//start app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
